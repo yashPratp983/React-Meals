e1dce478ac535e1163243f12039ce4ed96bd78f2
yashPratp983 <yash.here07@gmail.com>
2022-10-09 18:00:24 +0530

    added backend services throung firebase

--- src/App.jsx
+++ src/App.jsx
@@ -5,35 +5,32 @@ import CartModal from './components/UI/cartModa'
 import Cart from './components/UI/cart'
 import Qantity from './components/UI/quantity'
 import { useEffect } from 'react'
+import useApi from './hooks/useApi'
 // import { cart } from './components/UI/button'
 function App() {
   const [cartShow, setCartShow] = useState(false);
   const [counter, setCounter] = useState(JSON.parse(localStorage.getItem(1)) + JSON.parse(localStorage.getItem(2)) + JSON.parse(localStorage.getItem(3)) + JSON.parse(localStorage.getItem(4)));
-  const menu = [{
-    id: 1,
-    name: 'Sushi',
-    price: '$22.98',
-    description: 'finnest fish and veggies',
-    quantity: JSON.parse(localStorage.getItem(1)),
-  }, {
-    id: 2,
-    name: 'Schnitzel',
-    price: '$16.50',
-    description: 'A german speciality',
-    quantity: JSON.parse(localStorage.getItem(2)),
-  }, {
-    id: 3,
-    name: 'Barbecue Burger',
-    price: '$12.99',
-    description: 'American raw and meaty',
-    quantity: JSON.parse(localStorage.getItem(3)),
-  }, {
-    id: 4,
-    name: 'Green Bowl',
-    price: '$18.56',
-    description: 'Healthy...green..',
-    quantity: JSON.parse(localStorage.getItem(4)),
-  }]
+  const [tasks, setTasks] = useState([]);
+
+  const { isLoading, error, sendRequest: fetchTasks } = useApi();
+
+  useEffect(() => {
+    const transformTasks = (tasksObj) => {
+      const loadedTasks = [];
+
+      for (let taskKey = 1; taskKey <= 4; taskKey++) {
+        loadedTasks.push({ id: taskKey, description: tasksObj[taskKey].description, name: tasksObj[taskKey].name, price: tasksObj[taskKey].price });
+      }
+      console.log(tasksObj)
+
+      setTasks(loadedTasks);
+    };
+
+    fetchTasks(
+      { url: 'https://food-order-app-51714-default-rtdb.firebaseio.com/menu.json' },
+      transformTasks
+    );
+  }, [fetchTasks]);
 
   const cartShowHandler = () => {
     console.log(cartShow)
@@ -55,9 +52,9 @@ function App() {
         <Cart.Provider value={{
           openCart: cartShow
         }}>
-          <CartModal cartShow={cartShowHandler} menu={menu} counter={counterHandler} />
+          <CartModal cartShow={cartShowHandler} menu={tasks} counter={counterHandler} />
           <Header cartShow={cartShowHandler} />
-          <Menu menu={menu} reload={cartShow} counter={counterHandler} />
+          <Menu error={error} isLoading={isLoading} menu={tasks} reload={cartShow} counter={counterHandler} />
         </Cart.Provider>
       </Qantity.Provider>
     </React.Fragment>
